349e5a4e30d0225e36b4c68d7e07ef50ba7f175d
mik <mail@kissel.ch>
2020-05-22 22:28:01 +0200

    work on

--- urlwatcher.py
+++ urlwatcher.py
@@ -1,7 +1,23 @@
+import os
+
 urls = [
-('google', 'https://google.ch'),
+('Google', 'https://google.ch'),
+('kissel.ch', 'https://kissel.ch/site'),
 ('SBB', 'https://sbb.ch')
 ]
 
-for domain,url in urls:
-    print(domain, url)
+def printConfiguredUrl():
+    for domain,url in urls:
+        print(domain, url)
+
+def manageDirs():
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
+def createImages():
+
+
+
+manageDirs()
+
+#rintConfiguredUrl()
