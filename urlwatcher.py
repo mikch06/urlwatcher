ac917d1bc0ea59fb00c6ea46900f3d68d6728921
mik <mail@kissel.ch>
2020-06-22 22:45:11 +0200

    revision

--- urlwatcher.py
+++ urlwatcher.py
@@ -1,14 +1,11 @@
 import os
+import urls
 
-urls = [
-('Google', 'https://google.ch'),
-('kissel.ch', 'https://kissel.ch/site'),
-('SBB', 'https://sbb.ch')
-]
 
 def printConfiguredUrl():
-    for domain,url in urls:
-        print(domain, url)
+    with open('urls.py', 'r') as urls:
+        for domain,url in urls:
+            print(domain, url)
 
 def manageDirs():
     if not os.path.exists('tmp'):
@@ -18,5 +15,5 @@ def createImages():
 
 
 
-manageDirs()
-printConfiguredUrl()
+#manageDirs()
+#printConfiguredUrl()
